54cd793c01dbe92a26dead130c7feeec623e5275
mbene dieye <mbenedieye757@gmail.com>
2025-05-29 20:46:26 +0200

    catena di promessa simplice

--- index.js
+++ index.js
@@ -102,97 +102,107 @@
 
     // -------------------------catena di promessa simplice-------------------------------
 
-    function catenaDiPromessa(numero){
+    function primacatenaDiPromessa(numero){
         return new Promise((resolve)=>{
             setTimeout(()=>{
-                resolve("il numero è :", numero)
+                resolve(numero * 2)
             },1000)
-        }).then((result)=>{
-            result = (numero*2) + 3
-            console.log(result)
         })
+        
     }
-    catenaDiPromessa(4)
+    
+    function secondacatenaDiPromessa(numero){
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve(numero + 3)
+            },1000)
+        }) 
+    }
+    
+    primacatenaDiPromessa(5)
+    .then(res => secondacatenaDiPromessa(res))
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
 
     // ----------------------------------------catena di promessa con condizioni-------------------------------------
 
-    function promessaConCondizioni(numero){
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve("il numero è:",numero)
-            },1000)
-        }).then(()=>{
-          if(numero % 2===0){
-            console.log("il numero è pari")
-          }else{
-            console.log("il numero è dispari")
-          }
-        })
-    }
+    // function promessaConCondizioni(numero){
+    //     return new Promise((resolve)=>{
+    //         setTimeout(()=>{
+    //             resolve("il numero è:",numero)
+    //         },1000)
+    //     }).then(()=>{
+    //       if(numero % 2===0){
+    //         console.log("il numero è pari")
+    //       }else{
+    //         console.log("il numero è dispari")
+    //       }
+    //     })
+    // }
 
-    promessaConCondizioni(6)
+    // promessaConCondizioni(6)
 
-    // --------------------------------catena di promessa con gestioni degli errori------------------
+    // // --------------------------------catena di promessa con gestioni degli errori------------------
 
-    function gestioniDegliErrori(item){
-        return new Promise((resolve,reject)=>{
-            if(item){
-                resolve("valore reisolvato")
-            }else{
-                reject("valore rifiutato")
-            }
-        }).then((resultato)=>{
-            console.log(resultato)
-        }),(error)=>{
-            console.error(error)
-        }
-    }
-    gestioniDegliErrori("ciao")
+    // function gestioniDegliErrori(item){
+    //     return new Promise((resolve,reject)=>{
+    //         if(item){
+    //             resolve("valore reisolvato")
+    //         }else{
+    //             reject("valore rifiutato")
+    //         }
+    //     }).then((resultato)=>{
+    //         console.log(resultato)
+    //     }),(error)=>{
+    //         console.error(error)
+    //     }
+    // }
+    // gestioniDegliErrori("ciao")
 
-    // -----------------------------------------------gestioni degli errori con catch-----------------
+    // // -----------------------------------------------gestioni degli errori con catch-----------------
 
-    // function gestioniDegliErroriConCatch(){
-    //     return new Promise((reject)=>{
-    //         reject("promessa refiutata")
+    // // function gestioniDegliErroriConCatch(){
+    // //     return new Promise((reject)=>{
+    // //         reject("promessa refiutata")
+    // //     }).then((result)=>{
+    // //         console.log(result)
+    // //     }).catch((error)=>{
+    // //         console.error(error)
+    // //     })
+    // // }
+
+    // // gestioniDegliErroriConCatch()
+
+    // // // ----------------------------------gestioni degli errori con then e catch-------------------------------------
+
+    // // function gestioniDegliErroriConThenCatch(num){
+    // //     return new Promise((resolve,reject)=>{
+    // //         if(num == true){
+    // //             resolve("promessa resolvata")
+    // //         }else{
+    // //             reject("promessa refuitato")
+    // //         }
+    // //     }).then((result)=>{
+    // //         console.log(result)
+    // //     }).catch((errore)=>{
+    // //         console.error(errore)
+    // //     })
+    // // }
+    // // gestioniDegliErroriConThenCatch("ciao")
+
+    // // ----------------------------gestioni degli errori con una catena di promessa---------------------
+
+    // function gestioniDegliErroriConCatenaDiPromessa(){
+    //     return new Promise((resolve,reject)=>{
+    //         resolve("valore resolvata")
+    //         reject("valore refuitata")
     //     }).then((result)=>{
     //         console.log(result)
     //     }).catch((error)=>{
     //         console.error(error)
     //     })
     // }
-
-    // gestioniDegliErroriConCatch()
-
-    // // ----------------------------------gestioni degli errori con then e catch-------------------------------------
-
-    // function gestioniDegliErroriConThenCatch(num){
-    //     return new Promise((resolve,reject)=>{
-    //         if(num == true){
-    //             resolve("promessa resolvata")
-    //         }else{
-    //             reject("promessa refuitato")
-    //         }
-    //     }).then((result)=>{
-    //         console.log(result)
-    //     }).catch((errore)=>{
-    //         console.error(errore)
-    //     })
-    // }
-    // gestioniDegliErroriConThenCatch("ciao")
-
-    // ----------------------------gestioni degli errori con una catena di promessa---------------------
-
-    function gestioniDegliErroriConCatenaDiPromessa(){
-        return new Promise((resolve,reject)=>{
-            resolve("valore resolvata")
-            reject("valore refuitata")
-        }).then((result)=>{
-            console.log(result)
-        }).catch((error)=>{
-            console.error(error)
-        })
-    }
-    gestioniDegliErroriConCatenaDiPromessa()
+    // gestioniDegliErroriConCatenaDiPromessa()
 
     // ------------------utiliasare promise all--------------------------------
 
@@ -271,27 +281,27 @@
 
     // -----------------------------------------funziona asincrona simplice-----------------------------
 
-     function asincronaSimplice(resultato) {
-       try {
-             return new Promise((resolve)=>{
-             setTimeout(()=>{
-                resolve(resultato)
-            },2000)
-        }) 
-       } catch (error) {
-            console.log(error)
-       }
-    }
+//      function asincronaSimplice(resultato) {
+//        try {
+//              return new Promise((resolve)=>{
+//              setTimeout(()=>{
+//                 resolve(resultato)
+//             },2000)
+//         }) 
+//        } catch (error) {
+//             console.log(error)
+//        }
+//     }
 
-    async function init() {
-        let res = await asincronaSimplice("buonasera")
-        console.log(" il resultato è :"+res)
-    }
-init()
+//     async function init() {
+//         let res = await asincronaSimplice("buonasera")
+//         console.log(" il resultato è :"+res)
+//     }
+// init()
 
-// ----------------------------------------gestione degli errori con try e catch-----------------------
+// // ----------------------------------------gestione degli errori con try e catch-----------------------
 
-// async function erroriTryCatch() {
+// // async function erroriTryCatch() {
 //     try {
 //         // ho utilisato la functione -gestioni degli errori con then e catch-
 //         let resultat = await gestioniDegliErroriConThenCatch()
@@ -305,35 +315,35 @@ init()
 
 // --------------------------------funzione asincrone in serie--------------------
 
-async function asincroneInSerieUno() {
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve("asincroneInSerieUno risolvata")
-        },2000)
-    }).then((result)=>{
-        console.log(result)
-    })
-}
-asincroneInSerieUno()
+// async function asincroneInSerieUno() {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve("asincroneInSerieUno risolvata")
+//         },2000)
+//     }).then((result)=>{
+//         console.log(result)
+//     })
+// }
+// asincroneInSerieUno()
 
-async function asincroneInSerieDue() {
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve("asincroneInSerieDue risolvata")
-        },3000)
-    }).then((result)=>{
-        console.log(result)
-    })
-}
-asincroneInSerieDue()
+// async function asincroneInSerieDue() {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve("asincroneInSerieDue risolvata")
+//         },3000)
+//     }).then((result)=>{
+//         console.log(result)
+//     })
+// }
+// asincroneInSerieDue()
 
-async function asincroneInSerieTre() {
-    let result1 = await asincroneInSerieUno()
-    console.log(result1)
-    let result2 = await asincroneInSerieDue()
-    console.log(result2)
-}
-asincroneInSerieTre()
+// async function asincroneInSerieTre() {
+//     let result1 = await asincroneInSerieUno()
+//     console.log(result1)
+//     let result2 = await asincroneInSerieDue()
+//     console.log(result2)
+// }
+// asincroneInSerieTre()
 
 // ---------------------------eseguire una richiesta get simplice----------------
 
